f96a3ae73024631787fe2dcc4a8087e1d412ae0a
dodaa08 <dodakartik26@gmail.com>
2024-11-21 23:51:40 +0530

    Added working orgs

--- frontend/pages/VendorPage.tsx
+++ frontend/pages/VendorPage.tsx
@@ -1,17 +1,9 @@
 export default function VendorPage() {
   const vendors = [
-    { name: 'Hulu', image: '/images/hulu.png', link: '/vendors/hulu' },
-    { name: 'Adobe', image: '/images/adobe.png', link: '/vendors/adobe' },
+ 
     { name: 'Bandcamp', image: '/images/bandcamp.png', link: '/vendors/bandcamp' },
     { name: 'Udemy', image: '/images/udemy.png', link: '/vendors/udemy' },
-    { name: 'Thingverse', image: '/images/thingverse.png', link: '/vendors/thingverse' },
-    { name: 'Mycybersale', image: '/images/mycybersale.png', link: '/vendors/mycybersale' },
-    { name: 'Gumroad', image: '/images/gumroad.png', link: '/vendors/gumroad' },
-    { name: 'Steam', image: '/images/steam.png', link: '/vendors/steam' },
-    { name: 'Skillshare', image: '/images/skillshare.png', link: '/vendors/skillshare' },
-    { name: 'Google', image: '/images/google.png', link: '/vendors/google' },
-    { name: 'Envato', image: '/images/envato.png', link: '/vendors/envato' },
-    { name: 'Appsbee', image: '/images/appsbee.png', link: '/vendors/appsbee' },
+
   ];
 
   return (
